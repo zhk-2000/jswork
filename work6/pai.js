75ddf9418b366b856c309611960b2decc4857fc7
zhk-2000 <1371417965@qq.com>
2019-10-17 10:00:55 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,13 @@
+function calculate(){
+    let num = document.getElementById('num').value
+    num = parseInt(num)&&Number(num)
+    if(!IDBTransaction(num)){
+        document.getElementById('pai').value = pi(num)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1<=n;++i){
+s
+    }
+}
