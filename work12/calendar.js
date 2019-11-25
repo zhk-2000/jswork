c64fdaa15e3b8fec032f9225083f91ade989108c
apple <apple@jd505-05.local>
2019-11-25 11:56:52 +0800

    work12

---
+++ work12/calendar.js
@@ -0,0 +1,35 @@
+//定义年历生成函数
+function calendar(y) { 
+    // 获取指定年份1月1 日的星期数值
+    var W = new Date(y, 0).getDay();
+    
+    var html = '<div class="box">';
+    
+    //拼接每个月份的表格
+    for(varm=1;m<=12;++m) {
+        html += '<table>';
+        html += '<tr class="title"><th colspan="7">'+ y + '年' + m + '月</th></tr>';
+        html += '<tr><td> 日</ td><td>-</td><td>= </ td><td>三</ td><td>四</ td><td>五</ td><td>六</td></tr>';
+    
+    //获取月份m共有多少天
+    var max = new Date(y, m, 0).getDate();
+    
+    html += '<tr>'; //开始<tr>标签
+    for(vard=1;d<=max;++d){
+        if (w && d == 1) { //如果该月的第1天不是星期日，则填充空白
+    html += '<td colspan="' + W + '"> </td>';
+        }
+    html += '<td>' + d + '</td>' ;
+    
+    if (W == 6 && d != max){  //如果星期六不是该月的最后一天，则换行
+        html += '</tr><tr>';
+    }else if (d == max) { //该月的最后一天，闭合<tr>标签
+        html += '</tr>';
+    }
+    w = (w + 1 > 6) ? 0 : w + 1;
+}    
+    html += '</table>';   
+    }
+    html += '</div>' ;
+    return html;
+}
