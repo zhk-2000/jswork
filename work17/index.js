6f46d3a0ad04c0f45a7815d8ff348f17c11a684a
zhk-2000 <1371417965@qq.com>
2019-12-22 09:09:13 +0800

    work17

--- work17/index.js
+++ work17/index.js
@@ -1,5 +1,5 @@
-var inputs = document.getElementsByClassName('input');
-for (var i = 0; i < inputs.length - 1; i++){
+var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length - 1; ++i) {
     inputs[i].onblur = inputBlur;
 }
 function inputBlur() {
@@ -9,34 +9,48 @@ function inputBlur() {
     var tips_obj = this.parentNode.nextElementSibling;
     val = val.trim();
     if (!val) {
-        error(tips_obj, '输入框不能为空');
-        return false;
+        error(tips_obj, '输入框不能为空')
+        return false
     }
     var reg_msg = getRegMsg(name, tips);
     if (reg_msg['reg'].test(val)) {
-        success(tips_obj, reg_msg['mag']['success']);
-    }else{
-        errpr(tips_obj,reg_msg['mas']['error']);
+        success(tips_obj, reg_msg['msg']['success']);
+    } else {
+        error(tips_obj, reg_msg['msg']['error']);
     }
 }
 function getRegMsg(name, tips) {
-    var reg =msg = '';
+    var reg = msg = '';
     switch (name) {
         case 'username':
-            reg = /^[a-zA-Z]{4,14}$/;
-            msg = {'success': '用户名输入正确','error': tips};
+            reg = /^[a-zA-Z]{4,12}$/;
+            msg = {'success': '用户名输入正确', 'error': tips};
             break;
-            case 'pwd':
-                reg = /^\w{6,20}$/;
-                msg = {'success': '密码输入正确','error':tips};
-                break;
-                case 'repwd':
-                    var con = document.getElementsByClassName('input')[1].value;
-                    reg = RegExp("^" + con + "$");
-                    msg = {'success':'两次密码输入正确','error':'两次输入的密码不一致'};
-                    break;
-                    case 'tel':
-                        reg = /^1[34578]\d{9}$/;
-                        msg = {'success': '手机号码输入正确','error': tips};
+        case 'pwd':
+            reg = /^\w{6,20}$/;
+            msg = {'success': '密码输入正确', 'error': tips};
+            break;
+        case 'repwd':
+            var con = document.getElementsByTagName('input')[1].value;
+            reg = RegExp("^" + con + "$");
+            msg = {'success': '两次密码输入正确', 'error': '两次输入的密码不一致'};
+            break;
+        case 'tel':
+            reg = /^1[34578]\d{9}$/;
+            msg = {'success': '手机号码输入正确', 'error': tips};
+            break;
+        case 'email':
+            reg = /^(\w+(\_|\-|\.)*)+@(\w+(\-)?)+(\.\w{2,})+$/;
+            msg = {'success': '邮箱输入正确', 'error': tips};
+            break;
     }
+    return {'reg': reg, 'msg': msg};
+}
+function success(obj, msg) {
+    obj.className = 'success';
+    obj.innerHTML = msg;
+}
+function error(obj, msg) {
+    obj.className = 'error';
+    obj.innerHTML = msg + '，请重新输入';
 }
